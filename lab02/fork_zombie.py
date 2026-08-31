f661c69927daef973aa06e3635a401d69ba23813
wasutorn aungsupanich <188597195+chinoposd@users.noreply.github.com>
2026-08-31 04:28:48 +0000

    Completed Lab 2 coding

---
+++ lab02/fork_zombie.py
@@ -0,0 +1,31 @@
+# fork_zombie.py
+
+import os
+import time
+import sys
+
+def main():
+    print(f"[Parent] My PID is {os.getpid()}")
+    print("[Parent] Forking a child process...")
+
+    # OS System Call: fork() creates an exact copy of the process
+    pid = os.fork()
+
+    if pid > 0:
+        # --- PARENT PROCESS ---
+        print(f"[Parent] Created Child with PID {pid}.")
+        print("[Parent] I am doing heavy ML work and forgot to call os.wait()...")
+        print("[Parent] Open another terminal and run: htop (Look for 'Z' status)")
+
+        time.sleep(60)
+
+        print("[Parent] Waking up and exiting.")
+
+    elif pid == 0:
+        # --- CHILD PROCESS ---
+        print(f"[Child] My PID is {os.getpid()}. I am finishing my task quickly!")
+
+        sys.exit(0)
+
+if __name__ == "__main__":
+    main()
